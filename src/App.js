e1ad0f39571f8cf4f4d387f959a6a26c46dc542f
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-15 04:44:11 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import './App.css';
-import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
 import app from './firebase/firebase.init';
+import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
+import { useState } from 'react';
 
-const auth = getAuth(app);
+const auth = getAuth(app)
 
 
 function App() {
   const provider =new GoogleAuthProvider()
+  const gitProvider = new GithubAuthProvider()
+  const [user, setUser] = useState({})
 
-const handleGoogleSingIn = () =>{
-  
+ const handleSingIn = () =>{
   signInWithPopup(auth, provider)
+  .then( result =>{
+    const user = result.user
+    setUser(user)
+    console.log(user)
+
+  })
+  .catch(error =>{
+    console.error(error)
+  })
+ }
+ const singOut = () =>{
+    signOut(auth)
+    .then(() =>{
+      setUser({})
+    })
+    .catch(() =>{
+      setUser({})
+    })
+ }
+ const gitHubSingIn = () =>{
+  signInWithPopup(auth, gitProvider)
   .then(result =>{
     const user = result.user
+    setUser(user)
     console.log(user)
   })
   .catch(error =>{
     console.error(error)
   })
-}
-// const handleSingOut = () =>{
-//   signOut(auth, provider)
-//   .then(result =>{
-//     const user = result.user
-//     console.log(user)
-//   })
-//   .catch(error =>{
-//     console.error(error)
-//   })
-// }
+ }
   
   return (
     <div className="App">
-    <button onClick={handleGoogleSingIn}>Google Sing in</button>
-    {/* <button onClick={handleSingOut}>sing out</button> */}
+  { user.uid ? 
+<button onClick={singOut}>Sing Out</button>
+:
+  <>
+    <button onClick={handleSingIn}>Sing In </button>
+    <button onClick={gitHubSingIn} >Github LogIn</button>
+  </>
+  }
+  {user.uid &&
+    <div>
+    <h2>Name: {user.displayName} </h2>
+    <h3>Email: {user.email} </h3>
+    <img src={user.photoURL} alt="" />
+  </div>}
     </div>
   );
 }
